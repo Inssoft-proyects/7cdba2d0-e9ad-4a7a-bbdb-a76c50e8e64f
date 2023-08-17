6960fe26e968e230a5304bed86cafb4782bfd82b
ivaninssoft <ivan.ortiz@inssoftmx.com>
2023-08-17 17:04:12 -0600

    log

--- Entity/DBContextInssoft.cs
+++ Entity/DBContextInssoft.cs
@@ -73,7 +73,7 @@ public partial class DBContextInssoft : DbContext
 
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    => optionsBuilder.UseSqlServer("Data Source=inssoft-sqlserver.database.windows.net;Database=sitteg-qa;User Id=sqladmin;Password=Inss0f7#2023;Trusted_Connection=False;TrustServerCertificate=True");
+    => optionsBuilder.UseSqlServer("Data Source=10.16.157.141;Database=sitteg;User Id=sa;Password=2023Jun10.;Trusted_Connection=False;TrustServerCertificate=True");
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Dependencias>(entity =>
